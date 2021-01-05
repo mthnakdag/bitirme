bf511e4c7b9a73991ef3439d6c614bda30ff0c08
metehan akdag <mthnakdag@gmail.com>
2021-01-05 10:46:28 +0300

    modal template added

--- client/src/js/CallWindow.js
+++ client/src/js/CallWindow.js
@@ -1,14 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
+import React, { useState, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import { Modal, Button } from "react-bootstrap";
 
-const getButtonClass = (icon, enabled) => classnames(`btn-action fa ${icon}`, { disable: !enabled });
+const getButtonClass = (icon, enabled) =>
+  classnames(`btn-action fa ${icon}`, { disable: !enabled });
 
-function CallWindow({ peerSrc, localSrc, config, mediaDevice, status, endCall }) {
+function CallWindow({
+  peerSrc,
+  localSrc,
+  config,
+  mediaDevice,
+  status,
+  endCall,
+}) {
   const peerVideo = useRef(null);
   const localVideo = useRef(null);
   const [video, setVideo] = useState(config.video);
   const [audio, setAudio] = useState(config.audio);
+  const [showSetting, setShowSetting] = useState(false);
+
+  const showModal = () => {
+    console.log(showSetting);
+    setShowSetting(!showSetting);
+  };
 
   useEffect(() => {
     if (peerVideo.current && peerSrc) peerVideo.current.srcObject = peerSrc;
@@ -17,8 +32,8 @@ function CallWindow({ peerSrc, localSrc, config, mediaDevice, status, endCall })
 
   useEffect(() => {
     if (mediaDevice) {
-      mediaDevice.toggle('Video', video);
-      mediaDevice.toggle('Audio', audio);
+      mediaDevice.toggle("Video", video);
+      mediaDevice.toggle("Audio", audio);
     }
   });
 
@@ -27,32 +42,54 @@ function CallWindow({ peerSrc, localSrc, config, mediaDevice, status, endCall })
    * @param {String} deviceType - Type of the device eg: Video, Audio
    */
   const toggleMediaDevice = (deviceType) => {
-    if (deviceType === 'video') {
+    if (deviceType === "video") {
       setVideo(!video);
-      mediaDevice.toggle('Video');
+      mediaDevice.toggle("Video");
     }
-    if (deviceType === 'audio') {
+    if (deviceType === "audio") {
       setAudio(!audio);
-      mediaDevice.toggle('Audio');
+      mediaDevice.toggle("Audio");
     }
   };
 
   return (
-    <div className={classnames('call-window', status)}>
+    <div className={classnames("call-window", status)}>
+      {/* DEFAULT ON THE BELOW */}
       <video id="peerVideo" ref={peerVideo} autoPlay />
       <video id="localVideo" ref={localVideo} autoPlay muted />
+      {showSetting && (
+        <Modal
+          style={{ opacity: "1", color: "black" }}
+          show={showSetting}
+          onHide={showModal}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={showModal}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={showModal}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+
       <div className="video-control">
         <button
           key="btnVideo"
           type="button"
-          className={getButtonClass('fa-video-camera', video)}
-          onClick={() => toggleMediaDevice('video')}
+          className={getButtonClass("fa-video-camera", video)}
+          onClick={() => toggleMediaDevice("video")}
         />
         <button
           key="btnAudio"
           type="button"
-          className={getButtonClass('fa-microphone', audio)}
-          onClick={() => toggleMediaDevice('audio')}
+          className={getButtonClass("fa-microphone", audio)}
+          onClick={() => toggleMediaDevice("audio")}
         />
         <button
           type="button"
@@ -62,6 +99,7 @@ function CallWindow({ peerSrc, localSrc, config, mediaDevice, status, endCall })
         <button
           type="button"
           className="btn-action setting fa fa-cog"
+          onClick={() => showModal()}
         />
       </div>
     </div>
@@ -74,10 +112,10 @@ CallWindow.propTypes = {
   peerSrc: PropTypes.object, // eslint-disable-line
   config: PropTypes.shape({
     audio: PropTypes.bool.isRequired,
-    video: PropTypes.bool.isRequired
+    video: PropTypes.bool.isRequired,
   }).isRequired,
   mediaDevice: PropTypes.object, // eslint-disable-line
-  endCall: PropTypes.func.isRequired
+  endCall: PropTypes.func.isRequired,
 };
 
 export default CallWindow;
